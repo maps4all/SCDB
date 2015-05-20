6cb2ccbbdfbaa2969e2217bb6803e2a20f134815
maps4all <maps4all007@yandex.com>
2015-05-20 10:36:07 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,18 @@
+{
+    "moduleId" : "maps4all/SCDB",
+    "title" : "SCDB",
+    "subtitle" : "www.scdb.info",
+    
+    "backButton" : true,
+    "enableGPS" : false,
+    "zoomControl" : false,
+    "screenLockRot" : false,
+    "reload" : true,
+    
+    "shouldOverrideUrlLoading1" : "http",
+    "shouldOverrideUrlLoading2" : "https",
+
+    "loadUrl" : "http://map.scdb.info/speedcameramap/ll/#lat#,#lng#/z/#zoom#",
+   			
+    "browserLaunchLink" : "http://map.scdb.info/speedcameramap/ll/#lat#,#lng#/z/#zoom#"
+}
